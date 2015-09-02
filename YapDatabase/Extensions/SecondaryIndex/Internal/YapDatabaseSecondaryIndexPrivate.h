37b16f00c34850041b0b3a716ae01b3aafc2ff7a
Ngoc Luu <ngocluu.net@gmail.com>
2015-09-02 08:55:09 +0700

    Add support for secondary index searching to YapDatabaseSearchResultsView extension.

--- YapDatabase/Extensions/SecondaryIndex/Internal/YapDatabaseSecondaryIndexPrivate.h
+++ YapDatabase/Extensions/SecondaryIndex/Internal/YapDatabaseSecondaryIndexPrivate.h
@@ -115,4 +115,8 @@
 - (id)initWithSecondaryIndexConnection:(YapDatabaseSecondaryIndexConnection *)secondaryIndexConnection
                    databaseTransaction:(YapDatabaseReadTransaction *)databaseTransaction;
 
+- (BOOL)_enumerateRowidsMatchingQuery:(YapDatabaseQuery *)query
+                           usingBlock:(void (^)(int64_t rowid, BOOL *stop))block;
+- (BOOL)rowid:(int64_t)rowid matches:(YapDatabaseQuery *)query;
+
 @end
